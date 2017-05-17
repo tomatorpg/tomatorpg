2c2e6886904e91b81278ca7a4f6e5bf3b2592e70
Koala Yeung <koalay@gmail.com>
2017-05-17 17:13:14 +0800

    Refactor webpack config for css generation
    * Extracts CSS as ./public/assets/css/*
    * Better NODE_ENV handling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,67 @@
+'use strict';
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const webpack = require('webpack');
 const path = require('path');
 const url = require('url');
 
 require('dotenv').config();
 
-const hostStr = (process.env.WEBPACK_DEV_SERVER_HOST !== undefined) ?
-  process.env.WEBPACK_DEV_SERVER_HOST : 'http://localhost:8080';
-const port = (hostStr !== undefined && url.parse(hostStr).port) ?
-  url.parse(hostStr).port : '8080';
-const hostname = (hostStr !== undefined && url.parse(hostStr).hostname) ?
-  url.parse(hostStr).hostname : '8080';
+const isDev = process.env.NODE_ENV === 'development';
+
+function getScriptHost() {
+  const hostStr = (process.env.WEBPACK_DEV_SERVER_HOST !== undefined) ?
+    process.env.WEBPACK_DEV_SERVER_HOST : 'http://localhost:8080';
+  const parsed = url.parse(hostStr);
+  const publicPath = parsed.href + 'assets';
+  return {
+    publicPath,
+    url: url.parse(hostStr),
+  };
+}
+
+const extractSass = new ExtractTextPlugin({
+  filename: "css/[name].[contenthash].css",
+  disable: !isDev,
+});
+
+const plugins = isDev ? [
+  new webpack.HotModuleReplacementPlugin(),
+  extractSass,
+] : [
+  extractSass,
+];
+
+const sassRule = isDev ? {
+  test: /\.scss$/,
+  use: [{
+      loader: "style-loader" // creates style nodes from JS strings
+  }, {
+      loader: "css-loader" // translates CSS into CommonJS
+  }, {
+      loader: "sass-loader" // compiles Sass to CSS
+  }]
+} : {
+  test: /\.scss$/,
+  use: extractSass.extract({
+    use: [{
+      loader: "css-loader"
+    }, {
+      loader: "sass-loader"
+    }],
+    // use style-loader in development
+    fallback: "style-loader"
+  }),
+};
+
+const scriptHost = getScriptHost();
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
-    path: path.resolve(__dirname, 'public/assets/js'),
-    publicPath: 'http://' + path.join(`${hostname}:${port}`, 'assets/js'),
-    filename: 'common.js',
+    path: path.resolve(__dirname, 'public/assets'),
+    publicPath: !isDev ? '' : scriptHost.publicPath,
+    filename: 'js/common.js',
   },
   module: {
     rules: [
@@ -28,21 +73,10 @@ module.exports = {
           'babel-loader',
         ],
       },
-      {
-        test: /\.scss$/,
-        use: [{
-            loader: "style-loader" // creates style nodes from JS strings
-        }, {
-            loader: "css-loader" // translates CSS into CommonJS
-        }, {
-            loader: "sass-loader" // compiles Sass to CSS
-        }]
-      },
+      sassRule,
     ],
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  plugins: plugins,
   devServer: {
     hot: true, // this enables hot reload
     //hotOnly: true, // do not reload browser if hot reload failed
@@ -52,8 +86,8 @@ module.exports = {
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     },
-    host: hostname,
-    port: port,
+    host: scriptHost.url.hostname,
+    port: scriptHost.url.port,
     contentBase: path.join(__dirname, "public"),
     watchOptions: {
       poll: false,
