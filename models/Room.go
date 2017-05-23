0e89998a8b6b198bd9df952c57def5e62f2e75a0
Koala Yeung <koalay@gmail.com>
2017-05-23 16:21:33 +0800

    Fix Room model json naming

--- models/Room.go
+++ models/Room.go
@@ -1,11 +1,14 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import "time"
 
 // Room stores information about a room
 type Room struct {
-	gorm.Model
+	ID        uint   `gorm:"primary_key" json:"id"`
 	Name      string `gorm:"type:varchar(255)"`
 	ShortName string `gorm:"type:varchar(255)"`
 	Password  string
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
 }
