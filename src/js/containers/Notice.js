76af3534a0c67a7a374aa385d36208dcd28ef490
Sardo <sardo.ip@sardo.work>
2017-05-28 20:27:12 +0800

    Add Notice conatiners (Testing)

---
+++ src/js/containers/Notice.js
@@ -0,0 +1,26 @@
+/* eslint
+react/forbid-prop-types: 'warn'
+*/
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Notice extends Component {
+    render() {
+        return (
+            <div className="notice-board">
+            Test
+            </div>
+        );
+    }
+}
+
+Notice.propTypes = {
+    visible: PropTypes.bool
+};
+
+Notice.defaultProps = {
+    visible: true
+};
+
+export default Notice;
