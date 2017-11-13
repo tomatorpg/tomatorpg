ba06e5a435613379eb2b6206960dce507c350321
Sardo Ip <sardo.ip@sardo.work>
2017-11-13 18:07:53 +0800

    Improve UI design of rooms list
    Add bootstrap style

--- src/js/containers/Rooms.js
+++ src/js/containers/Rooms.js
@@ -23,39 +23,47 @@ class Rooms extends Component {
     const { rooms = [] } = this.props;
     return (rooms.length > 0) ? (
       <div id="rooms">
-        <Notice>
-          <h1 style={{ textAlign: 'center' }}> Welcome to Tomato RPG. </h1>
-          <p style={{ marginLeft: '1em', marginRight: '1em' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Duis nec dapibus nulla. Etiam eleifend risus leo, eu scelerisque lorem posuere vel.</p>
-        </Notice>
-        <button type="button" onClick={evt => this.createRoom(evt)}>Create</button>
-        <ul className="rooms">
-          { rooms.map((room) => {
-            const key = `room-${room.id}`;
-            const roomDisplayName = (typeof room.name === 'string' && room.name.trim() !== '') ?
-            room.name : `Room ${room.id}`;
-            return (
-              <li key={key} className="room">
-                <Link className="room-name" to={`/rooms/${room.id}`}>
-                  {roomDisplayName}
-                </Link>
-                <div className="room-actions">
-                  <button type="button" onClick={() => this.joinRoom(room.id)}>Join</button>
-                </div>
-              </li>
-            );
-          }) }
-        </ul>
+        <div className="container">
+          <Notice>
+            <h1 style={{ textAlign: 'center' }}> Welcome to Tomato RPG. </h1>
+            <p style={{ marginLeft: '1em', marginRight: '1em' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Duis nec dapibus nulla. Etiam eleifend risus leo,
+              eu scelerisque lorem posuere vel.</p>
+          </Notice>
+          <div className="create-button">
+            <button className="btn btn-primary" type="button" onClick={evt => this.createRoom(evt)}>Create</button>
+          </div>
+          <ul className="rooms">
+            { rooms.map((room) => {
+              const key = `room-${room.id}`;
+              const roomDisplayName = (typeof room.name === 'string' && room.name.trim() !== '') ?
+              room.name : `Room ${room.id}`;
+              return (
+                <li key={key} className="room">
+                  <Link className="room-name" to={`/rooms/${room.id}`}>
+                    {roomDisplayName}
+                  </Link>
+                  <div className="room-actions">
+                    <button className="btn btn-primary" type="button" onClick={() => this.joinRoom(room.id)}>Join</button>
+                  </div>
+                </li>
+              );
+            }) }
+          </ul>
+        </div>
       </div>
     ) : (
       <div id="rooms">
-        <Notice>
-          <h1 style={{ textAlign: 'center' }}> Welcome to Tomato RPG. </h1>
-          <p style={{ marginLeft: '1em', marginRight: '1em' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Duis nec dapibus nulla. Etiam eleifend risus leo, eu scelerisque lorem posuere vel.</p>
-        </Notice>
-        <button type="button" onClick={evt => this.createRoom(evt)}>Create</button>
-        <p className="msg-no-room">There is no room yet.</p>
+        <div className="container">
+          <Notice>
+            <h1 style={{ textAlign: 'center' }}> Welcome to Tomato RPG. </h1>
+            <p style={{ marginLeft: '1em', marginRight: '1em' }}> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Duis nec dapibus nulla. Etiam eleifend risus leo,
+              eu scelerisque lorem posuere vel.</p>
+          </Notice>
+          <button className="btn btn-primary" type="button" onClick={evt => this.createRoom(evt)}>Create</button>
+          <p className="msg-no-room">There is no room yet.</p>
+        </div>
       </div>
     );
   }
